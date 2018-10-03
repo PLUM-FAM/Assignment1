3490626895d52c8fe222f3fb06dff44585715acf
Logan <Logand222@gmail.com>
2018-10-03 13:01:16 -0600

    Updated project with Andy's code

--- src/Driver.java
+++ src/Driver.java
@@ -1,6 +1,27 @@
-//Testing 
-public class Driver {
+
+import java.util.*;
+import java.io.*;
+
+public class Driver 
+{	
+	public static void main(String args[])
+	{
+		MazeReader reader = new MazeReader();
+
+		String[][] openMaze = reader.readOpen();
+		for(int i = 0; i < 20; i++)
+		{
+			for(int j = 0; j < 37; i++)
+			{
+				System.out.println(openMaze[i][j]);
+			}
+		}
+		
+		String[][] mediumMaze = reader.readMedium();
+		
+		String[][] largeMaze = reader.readLarge();
+		
+		
+	}
 
 }
-
-//joel
