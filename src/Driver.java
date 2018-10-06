a179beed9a4a66f98d6fa2b90f38b853a01a8f1b
Logan <Logand222@gmail.com>
2018-10-06 12:58:31 -0600

    DFS method and initializing mazes method

--- src/Driver.java
+++ src/Driver.java
@@ -6,6 +6,7 @@ public class Driver
 {	
 	
 	public static Queue<Node> q = new LinkedList<Node>();
+	public static Stack<Node> s = new Stack<>();
 	
 	public static int openMazeStartx;
 	public static int openMazeStarty;
@@ -34,18 +35,141 @@ public class Driver
 	
 	public static void main(String args[])
 	{
+		MazeReader reader = new MazeReader();
+		
+		//Reading all maze files and getting initial values
+		resetMazes(reader);
+		getStartandFinish();
+		
+//		/*
+//		 * ************************************
+//		 * OPEN MAZE BFS 
+//		 * ************************************
+//		 */
+//		
+//		
+//		Node n = bfs(openMaze, 37,20, openMazeStartx, openMazeStarty);
+//		for(int i = 0; i < 20; i++)
+//		{
+//			for(int j = 0; j < 37; j++)
+//			{
+//				System.out.print(openMaze[i][j]);
+//			}
+//			System.out.println("");
+//		}
+//		System.out.println("End of BFS - Cost for open maze.txt = " + bfsCost);
+//		
+//		
+//
+//		/*
+//		 * ************************************
+//		 * MEDIUM MAZE BFS 
+//		 * ************************************
+//		 */
+//		
+//		Node m = bfs(mediumMaze, 61,23, mediumMazeStartx, mediumMazeStarty);
+//		for(int i = 0; i < 23; i++)
+//		{
+//			for(int j = 0; j < 61; j++)
+//			{
+//				System.out.print(mediumMaze[i][j]);
+//			}
+//			System.out.println("");
+//		}
+//		System.out.println("End of BFS - Cost for medium maze.txt = " + bfsCost);
+//		
+//		
+//		/*
+//		 * ***********************************************************************
+//		 * LARGE MAZE BFS 
+//		 * ***********************************************************************
+//		 */
+//		
+//		
+//		Node o = bfs(largeMaze, 81, 31, largeMazeStartx, largeMazeStarty);
+//		for(int i = 0; i < 31; i++)
+//		{
+//			for(int j = 0; j < 81; j++)
+//			{
+//				System.out.print(largeMaze[i][j]);
+//			}
+//			System.out.println("");
+//		}
+//		System.out.println("End of BFS - Cost for large maze.txt = " + bfsCost);
+		
+//		resetMazes(reader);
+		
+	
+		
 		/*
-		 * ************************************
-		 * OPEN MAZE BFS 
-		 * ************************************
+		 * *************************************************************************
+		 * OPEN MAZE DFS 
+		 * *************************************************************************
 		 */
 		
-		MazeReader reader = new MazeReader();
-		openMaze = reader.readMaze("open maze.txt");
+		Node r = dfs(openMaze, 37,20, openMazeStartx, openMazeStarty);
 		for(int i = 0; i < 20; i++)
 		{
 			for(int j = 0; j < 37; j++)
+			{
+				System.out.print(openMaze[i][j]);
+			}
+			System.out.println("");
+		}
+		System.out.println("End of DFS - Cost for open maze.txt = " + dfsCost + "\n");
+		
+		
 
+		/*
+		 * *********************************************************************
+		 * MEDIUM MAZE DFS 
+		 * *********************************************************************
+		 */
+		
+		Node p = dfs(mediumMaze, 61,23, mediumMazeStartx, mediumMazeStarty);
+		for(int i = 0; i < 23; i++)
+		{
+			for(int j = 0; j < 61; j++)
+			{
+				System.out.print(mediumMaze[i][j]);
+			}
+			System.out.println("");
+		}
+		System.out.println("End of DFS - Cost for medium maze.txt = " + dfsCost + "\n");
+		
+		
+		/*
+		 * *********************************************************************
+		 * LARGE MAZE DFS 
+		 * *********************************************************************
+		 */
+		
+		
+		Node q = dfs(largeMaze, 81, 31, largeMazeStartx, largeMazeStarty);
+		for(int i = 0; i < 31; i++)
+		{
+			for(int j = 0; j < 81; j++)
+			{
+				System.out.print(largeMaze[i][j]);
+			}
+			System.out.println("");
+		}
+		System.out.println("End of DFS - Cost for large maze.txt = " + dfsCost + "\n");
+		
+		//reset all mazes after DFS
+		resetMazes(reader);
+	}
+
+
+
+
+	private static void getStartandFinish() {
+		
+		// finding open maze start and finish coordinates
+		for(int i = 0; i < 20; i++)
+		{
+			for(int j = 0; j < 37; j++)
+				
 			{	
 				//figure out start & finish state for open maze
 				if(openMaze[i][j] == 'P')//start
@@ -58,31 +182,11 @@ public class Driver
 					openMazeFinishx =j;
 					openMazeFinishy =i;
 				}
-
+				
 			}
 		}
 		
-		Node n = bfs(openMaze, 37,20, openMazeStartx, openMazeStarty);
-		for(int i = 0; i < 20; i++)
-		{
-			for(int j = 0; j < 37; j++)
-			{
-				System.out.print(openMaze[i][j]);
-			}
-			System.out.println("");
-		}
-		System.out.println("End of BFS - Cost for open maze.txt = " + bfsCost);
-		
-		
-
-		/*
-		 * ************************************
-		 * MEDIUM MAZE BFS 
-		 * ************************************
-		 */
-		
-		reader = new MazeReader();
-		mediumMaze = reader.readMaze("medium maze.txt");
+		// finding Medium maze start and finish coordinates
 		for(int i = 0; i < 23; i++)
 		{
 			for(int j = 0; j < 61; j++)
@@ -101,26 +205,7 @@ public class Driver
 			}
 		}
 		
-		Node m = bfs(mediumMaze, 61,23, mediumMazeStartx, mediumMazeStarty);
-		for(int i = 0; i < 23; i++)
-		{
-			for(int j = 0; j < 61; j++)
-			{
-				System.out.print(mediumMaze[i][j]);
-			}
-			System.out.println("");
-		}
-		System.out.println("End of BFS - Cost for medium maze.txt = " + bfsCost);
-		
-		
-		/*
-		 * ************************************
-		 * LARGE MAZE BFS 
-		 * ************************************
-		 */
-		
-		reader = new MazeReader();
-		largeMaze = reader.readMaze("large maze.txt");
+		// finding large maze start and finish coordinates
 		for(int i = 0; i < 31; i++)
 		{
 			for(int j = 0; j < 81; j++)
@@ -138,25 +223,19 @@ public class Driver
 				}	
 			}
 		}
-		
-		Node o = bfs(largeMaze, 81, 31, largeMazeStartx, largeMazeStarty);
-		for(int i = 0; i < 31; i++)
-		{
-			for(int j = 0; j < 81; j++)
-			{
-				System.out.print(largeMaze[i][j]);
-			}
-			System.out.println("");
-		}
-		System.out.println("End of BFS - Cost for large maze.txt = " + bfsCost);
-		
-		//reset all mazes after BFS
+	}
+
+
+
+
+	private static void resetMazes(MazeReader reader) {
 		openMaze = reader.readMaze("open maze.txt");
 		mediumMaze = reader.readMaze("medium maze.txt");
 		largeMaze = reader.readMaze("large maze.txt");
-	}
+	}	
 	
-		
+	
+	
 		
 	public static Node bfs(char[][] m, int mxbound, int mybound, int x, int y)
 	{
@@ -217,6 +296,64 @@ public class Driver
         }
         return null;
 	}
+	
+	public static Node dfs(char[][] m, int mxbound, int mybound, int x, int y)
+	{
+		dfsCost = 0; //reset cost for new instance
+		
+		s.add(new Node(x, y, null));
+
+        while(!s.isEmpty()) {
+        	
+            Node p = s.pop();
+
+            if(isFree(m, mxbound, mybound, p.getX()+1,p.getY())) 
+            {
+                Node nextP = new Node(p.getX()+1,p.getY(), p); //east
+                s.add(nextP);
+                bfsCost++;
+                if (m[nextP.getY()][nextP.getX()] == '*') { //goal test
+                    System.out.println("Exit is reached!");
+                    return nextP;
+                }
+                m[nextP.getY()][nextP.getX()] = '.';
+            }
+
+            if(isFree(m, mxbound,mybound, p.getX()-1,p.getY())) {
+                Node nextP = new Node(p.getX()-1,p.getY(), p); //west
+                s.add(nextP);
+                dfsCost++;
+                if (m[nextP.getY()][nextP.getX()] == '*') {//goal test
+                    System.out.println("Exit is reached!");
+                    return nextP;
+                }
+                m[nextP.getY()][nextP.getX()] = '.';
+            }
+
+            if(isFree(m,mxbound,mybound, p.getX(),p.getY()+1)) {
+                Node nextP = new Node(p.getX(),p.getY()+1, p); //south
+                s.add(nextP);
+                dfsCost++;
+                if (m[nextP.getY()][nextP.getX()] == '*') {//goal test
+                    return nextP;//exit is reached
+                }
+                m[nextP.getY()][nextP.getX()] = '.';
+            }
+
+             if(isFree(m,mxbound,mybound, p.getX(),p.getY()-1)) {
+                Node nextP = new Node(p.getX(),p.getY()-1, p); //north
+                s.add(nextP);
+                dfsCost++;
+                if (m[nextP.getY()][nextP.getX()] == '*') {//goal test
+                    System.out.println("Exit is reached!");
+                    return nextP;
+                }
+                m[nextP.getY()][nextP.getX()] = '.';
+            }
+        }
+        return null;
+	}
+	
 	
     public static boolean isFree(char[][] m , int mxbound, int mybound, int x, int y) {
 		
